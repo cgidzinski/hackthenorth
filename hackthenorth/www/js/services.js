5c5b6ab6aeec4178c751efe41cde03e41cd2c09f
Colin Gidzinski <evg31337@gmail.com>
2016-09-17 05:19:40 -0400

    profile fixed

--- hackthenorth/www/js/services.js
+++ hackthenorth/www/js/services.js
@@ -12,20 +12,20 @@ angular.module('starter')
 
       signup: function(name, email, password) {
         $http.defaults.headers.common['x-access-token'] = window.localStorage['LOCAL_TOKEN_KEY'];
-        return $http.post("http://104.197.29.38/api/signup", {name: name,password: password,email: email});
+        return $http.post("http://104.197.29.38:8080/api/register", {name: name,password: password,email: email});
       },
 
 
    login: function(email, password) {
     $http.defaults.headers.common['x-access-token'] = window.localStorage['LOCAL_TOKEN_KEY'];
 
-        return $http.post("http://104.197.29.38/api/login", {email: email,password: password});
+        return $http.post("http://104.197.29.38:8080/api/login", {email: email,password: password});
       },
 
          profile: function() {
           $http.defaults.headers.common['x-access-token'] = window.localStorage['LOCAL_TOKEN_KEY'];
 
-          return $http.get("http://104.197.29.38/api/user");
+          return $http.get("http://104.197.29.38:8080/api/user");
       }
 
 
