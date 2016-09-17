2ea73625b69699aa487a8afe45668b19130bd546
Colin Gidzinski <evg31337@gmail.com>
2016-09-17 08:11:55 -0400

    map geo

--- hackthenorth/www/js/controllers.js
+++ hackthenorth/www/js/controllers.js
@@ -53,13 +53,27 @@ angular.module('starter.controllers', ['angular-jwt'])
 
 
 
-.controller('HomeCtrl', function($scope, $state, APIreq, $http) {
+.controller('HomeCtrl', function($scope, $state, APIreq, $http, $cordovaGeolocation) {
   $scope.$on('$ionicView.enter', function(e) {
     if (APIreq.tokenValid() == false) {
       $state.go('login');
     }
   });
-
+var options = {timeout: 10000, enableHighAccuracy: true};
+$cordovaGeolocation.getCurrentPosition(options).then(function(position){
+ 
+    var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+ 
+    var mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+ 
+    $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+      }, function(error){
+    console.log("Could not get location");
+  });
  })
 
 
